ee66f1ea6c2deebbc2ed6f206a42d711095e7749
Solaymane Himite <himitesolaymane@gmail.com>
2024-03-16 00:39:20 +0100

    initial commit

---
+++ src/main/java/perfect/modid/PerfectMod.java
@@ -0,0 +1,15 @@
+package perfect.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PerfectMod implements ModInitializer {
+	public static final String MOD_ID = "perfect_mod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/perfect/modid/PerfectModClient.java
@@ -0,0 +1,10 @@
+package perfect.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PerfectModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
